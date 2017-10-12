41ee037c7863f0da9884690d367a81e6c8e3fd5e
Ricky Roque <raroque29@hotmail.co>
2017-10-12 12:03:10 -0500

    finished pop quiz

--- PopQuiz.java
+++ PopQuiz.java
@@ -1,41 +1,57 @@
-package part_05;
-
 
 
 
 public class PopQuiz {
     public static void main(String[] args) {
 
-        int[] myArray = new int[10];
+        if(args.length == 0 ) {
 
-        myArray[0] = 1;
-        myArray[1] = 2;
-        myArray[2] = 3;
-        myArray[3] = 4;
-        myArray[4] = 5;
-        myArray[5] = 6;
-        myArray[6] = 7;
-        myArray[7] = 8;
-        myArray[8] = 9;
-        myArray[9] = 10;
+            int[] myArray = new int[10];
 
-        for (int i : myArray){
-            System.out.print(i);
+            myArray[0] = 1;
+            myArray[1] = 2;
+            myArray[2] = 3;
+            myArray[3] = 4;
+            myArray[4] = 5;
+            myArray[5] = 6;
+            myArray[6] = 7;
+            myArray[7] = 8;
+            myArray[8] = 9;
+            myArray[9] = 10;
+
+            for (int i : myArray) {
+                System.out.print(i);
+            }
+            
+
+            int temp;
+
+            for (int i = 0; i < myArray.length / 2; i++) { //divide .lenght by 2 otherwise it will do a 360
+                temp = myArray[myArray.length - i - 1];
+                myArray[myArray.length - i - 1] = myArray[i];
+                myArray[i] = temp;
+            }
+
+            for (int i : myArray) {
+                System.out.println(i);
+            }
         }
-        System.out.println();
 
-        int temp;
+        else {
 
-        for (int i = 0; i < myArray.length/2; i++) { //divide .lenght by 2 otherwise it will do a 360
-            temp = myArray[myArray.length - i - 1];
-            myArray[myArray.length -i - 1] = myArray[i];
-            myArray[i] = temp;
+
+            for (int i = 0; i < args.length / 2; i++) { //divide .lenght by 2 otherwise it will do a 360
+                String temp2;
+                temp2 = args[args.length - i - 1];
+                args[args.length - i - 1] = args[i];
+                args[i] = temp2;
+            }
+            for (String i : args) {
+                System.out.println(i);
+            }
+
         }
-
-        for (int i : myArray){
-            System.out.print(i);
-        }
-
     }
+
 
 }
